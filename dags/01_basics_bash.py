547509bae217877689fb249cb2c082ae1fe2f291
lemin37-dev <lemin37.dev@gmail.com>
2026-08-12 14:43:38 +0900

    Feat: DAG 구성 중간완료

--- dags/01_basics_bash.py
+++ dags/01_basics_bash.py
@@ -23,8 +23,27 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 # 2. DAG 정의 -> DAG 세션이 오픈
+# 2-1. DAG에서 활용할 default_args 정의
+# 시나리오
+# 작업 성공 -> 완료
+# 작업 실패 -> 5분 대기 -> 1회 재시도 -> 성공 -> 완료
+# 작업 실패 -> 5분 대기 -> 1회 재시도 -> 실패 -> 완료(실패)
+# 향후 작업이 재개되어도 누락된 과거 데이터 소급(Backfill) X
+default_args = {
+  "owner"           : "aic-de1-admin",      # DAG 소유자
+  "depends_on_past" : False,                # 과거 데이터(가동시간 대비) 소급 처리여부
+  "retries"         : 1,                    # 작업 실패 시 재시도 횟수
+  "retry_delay"     : timedelta(minutes=5), # 작업 실패 후 재시도 delay 정의
+}
+
 with DAG(
-  dag_id = "01_basics_bash"  # DAG 간 구분하는 용도
+  dag_id            = "01_basics_bash",  # DAG 간 구분하는 용도
+  description       = "DE 업무 중 배치 파이프라인 구성 중 데이터 프로세싱 오케스트레이션 담당 airflow의 DAG 작성 기본형",  # DAG 설명
+  default_args      = default_args, # 기본 인자값
+  schedule_interval = "@daily",  # 하루 1회 00시 00분 00초, 문자열, cron 표현
+  start_date        = datetime(2026,6,29),  # 현재 기준 갭이 발생해도 "depends_on_past" 값을 false로 설정해 소급적용 X
+  catchup           = False,   # 과거에 대한 소급 처리 실행 방지
+  tags              = ['bash', 'basic']   # DAG 검색(특정)을 위해 자유롭게 세팅
 ) as dag:
   # 3. Operator 정의
 
